69c985fa7fc933a808a16fefe865202bdae231c2
Aleksei Kvitinskii <aleksei.kvitinskii@gmail.com>
2019-07-05 11:10:43 +0300

    added hightlight on status panel when printer is printing

--- octoprint_zbolttheme/static/js/zbolttheme.js
+++ octoprint_zbolttheme/static/js/zbolttheme.js
@@ -1,20 +1,50 @@
 $(function() {
 
-    function ZBoltViewModel(parameters) {
+    function ZBConnectionViewModel(parameters) {
         var self = this;
 
-        var faviconUrl = document.querySelector("link[rel~='mask-icon-theme']").href
-        || link.href
-        || window.location.origin + "/favicon.ico";
+        // var faviconUrl = document.querySelector("link[rel~='mask-icon-theme']").href
+        // || link.href
+        // || window.location.origin + "/favicon.ico";
 
-        console.log('test');
+
+        self.onAfterBinding = function() {
+            var connection = $("#sidebar_plugin_klipper");
+            connection.collapse("hide");
+         }
     }
 
-    // OCTOPRINT_VIEWMODELS.push({
-    //     construct: ZBoltViewModel,
-    //     // dependencies: ["AppearanceViewModel"],
-    //     // elements: ["head"]
-    // });
+    OCTOPRINT_VIEWMODELS.push({
+        construct: ZBConnectionViewModel,
+        dependencies: ["connectionViewModel"]
+    });
+
+
+    function ZBStateViewModel(parameters) {
+        var self = this;
+        
+        self.printerStateViewModel = parameters[0];
+
+        self.printerStateViewModel.stateString.subscribe(function(p){
+            var s = $('#state_wrapper');
+
+            if (p == 'Printing') {
+                s.addClass('printing')
+            } else {
+                s.removeClass('printing')
+            }
+        });
+
+        self.onAfterBinding = function() {
+        }
+    }
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: ZBStateViewModel,
+        dependencies: ["printerStateViewModel"]
+    });
+
+
 
 
 
